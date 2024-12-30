25b53726b23b0650531374e3903fefc0bad95cb1
Helasdic <95930420+Helasdic@users.noreply.github.com>
2024-12-30 17:16:52 +0700

    masih dihome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
     <title>Rumah Tahfidh Quran</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
     @include('layouts.navbar') <!-- Menyertakan navbar -->
 
     <main>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,39 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">RTQ</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#visi-misi">Visi & Misi</a></li>
-                <li class="nav-item"><a class="nav-link" href="#program-kelas">Program</a></li>
-                <li class="nav-item"><a class="nav-link" href="#kegiatan">Kegiatan</a></li>
-                <li class="nav-item"><a class="nav-link" href="#daftar">Daftar</a></li>
-            </ul>
+      <a class="navbar-brand" href="#">RTQ</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#visi-misi">Visi & Misi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#program">Program</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kegiatan">Kegiatan</a>
+          </li>
+        </ul>
+        <div class="d-flex align-items-center">
+            <a href="https://instagram.com" class="text-dark me-2" target="_blank">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="https://youtube.com" class="text-dark" target="_blank">
+                <i class="bi bi-youtube"></i>
+            </a>
+            <a href="#daftar" class="btn btn-primary ms-2">Daftar</a>
         </div>
+      </div>
     </div>
-</nav>
+  </nav>
+  
